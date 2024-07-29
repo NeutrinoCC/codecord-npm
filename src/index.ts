09b32b320abfce5a798d28c3ed89a47998d5893e
unknown <jndrs@protonmail.com>
2024-07-29 16:31:22 +0200

    Nice interfaces with book class

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import Client from "./classes/client";
+import { Collector } from "./classes/collector";
+import { Book } from "./classes/book";
 
-export { Client };
+export { Client, Collector, Book };
